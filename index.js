6fac1ba2145d2f0d9383afef5fd203d91ab566b8
Joao Vitor <outlast4321@gmail.com>
2022-04-14 16:43:14 -0300

    retornando perguntas na linha de comando

--- index.js
+++ index.js
@@ -17,7 +17,13 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 //rotas
 app.get("/", (req, res) => {
-    res.render("index")
+    Pergunta.findAll({ raw: true }).then(perguntas => {
+        console.log(perguntas)
+        res.render("index", {
+            perguntas: perguntas
+        })
+    })
+
 })
 
 app.get("/perguntar", (req, res) => {
