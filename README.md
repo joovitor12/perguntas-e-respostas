5041b661d8eab5ab0820f56e4ab62fcfe3aa9448
Joao Vitor <outlast4321@gmail.com>
2022-04-14 11:35:23 -0300

    Local project created

---
+++ README.md
@@ -0,0 +1,2 @@
+# perguntas-e-respostas
+# perguntas-e-respostas
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express();
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar");
+})
+
+app.listen(8080, () => {
+    console.log("App rodando no localhost:8080")
+})
